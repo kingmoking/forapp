fa9205cb57b9417aff12cf98f6e3979ddae8207e
kingmoking <57092781+kingmoking@users.noreply.github.com>
2019-11-18 18:39:13 +0800

    Update Android.js

--- king/Android.js
+++ king/Android.js
@@ -1,5 +1,5 @@
 ﻿var Anapp =  {
     download:function(){
-        location.href = 'https://kingmoking.github.io/forapp/king/king1025.apk'
+        location.href = 'https://kingmoking.github.io/forapp/king/king1118.apk'
     }
 }
