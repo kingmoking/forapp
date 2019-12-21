7aff184d00f452d72841cd651db1770be70d9fa6
kingmoking <57092781+kingmoking@users.noreply.github.com>
2019-12-21 16:03:29 +0800

    Update Android.js

--- alsc/Android.js
+++ alsc/Android.js
@@ -1,5 +1,5 @@
 var Anapp =  {
     download:function(){
-        location.href = 'https://kingmoking.github.io/forapp/aslc/aslc.apk'
+        location.href = 'https://kingmoking.github.io/forapp/alsc/aslc.apk'
     }
 }
