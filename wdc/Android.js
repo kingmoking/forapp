8a9d56c333d791c4c3b5ee29edcacd600f4fc1a2
kingmoking <57092781+kingmoking@users.noreply.github.com>
2020-04-28 17:41:41 +0800

    Add files via upload

---
+++ wdc/Android.js
@@ -0,0 +1,5 @@
+﻿var Anapp =  {
+    download:function(){
+        location.href = 'https://kingmoking.github.io/forapp/WDC/adc0428.apk'
+    }
+}
---
+++ wdc/download.js
@@ -0,0 +1,40 @@
+var is_Android = true;
+// 判断安卓
+function isAndroid() {
+    var u = navigator.userAgent;
+    if (u.indexOf("Android") > -1 || u.indexOf("Linux") > -1) {
+        if (window.ShowFitness !== undefined) return true;
+    }
+    return false;
+};
+
+// 判断设备为 ios
+function isIos() {
+    var u = navigator.userAgent;
+    if (u.indexOf("iPhone") > -1 || u.indexOf("iOS") > -1) {
+        return true;
+    }
+    return false;
+};
+if (isAndroid() == true) {
+    is_Android = true;
+    $("#type").attr("src","./image/anzhuo.png");
+}
+if (isIos() == true) {
+    is_Android = false;
+    $("#type").attr("src","./image/iPhone.png");
+}
+
+//下载按钮事件
+$(".button").click(function () {
+    if (is_Android) {
+        console.log('Android');
+        //安卓下载
+        location.href = 'king1025.apk';
+    }
+    else {
+        console.log('ios');
+        //苹果
+        location.href = 'itms-services://?action=download-manifest&url=https://kingmoking.github.io/forapp/king/manifest.plist';
+    }
+})
