617c1335f4e989522c55055aa22ec25adbc9ea7d
kingmoking <57092781+kingmoking@users.noreply.github.com>
2020-05-27 16:24:55 +0800

    Update Android.js

--- wdc/Android.js
+++ wdc/Android.js
@@ -1,5 +1,5 @@
 ﻿var Anapp =  {
     download:function(){
-        location.href = 'https://kingmoking.github.io/forapp/wdc/adc0508.apk'
+        location.href = 'https://kingmoking.github.io/forapp/wdc/adc0527.apk'
     }
 }
