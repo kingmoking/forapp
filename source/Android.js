916dacf131eb170c0f9d9ce932a0a399c8817128
kingmoking <57092781+kingmoking@users.noreply.github.com>
2020-06-13 09:50:08 +0800

    Update Android.js

--- source/Android.js
+++ source/Android.js
@@ -1,5 +1,5 @@
 ﻿var Anapp =  {
     download:function(){
-        location.href = 'https://kingmoking.github.io/forapp/source/source1.0.4.apk'
+        location.href = 'https://kingmoking.github.io/forapp/source/source1.0.15.apk'
     }
 }
