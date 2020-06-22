3cb4c678a14b02573a972cbb6e4a01a3a6e4e179
kingmoking <57092781+kingmoking@users.noreply.github.com>
2020-06-22 18:50:05 +0800

    Update Android.js

--- sanpn/Android.js
+++ sanpn/Android.js
@@ -1,5 +1,5 @@
 ﻿var Anapp =  {
     download:function(){
-        location.href = 'https://kingmoking.github.io/forapp/sanpn/source1.0.0.apk'
+        location.href = 'https://kingmoking.github.io/forapp/sanpn/sanpn1.0.0.apk'
     }
 }
